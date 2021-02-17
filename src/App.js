f817e7c19bffd9d0f1e86751768beeba4468f637
Alfredo Rivas Jimenez <alfredo_rivas_jimenez@outlook.com>
2021-02-16 23:32:13 -0600

    [ADDED] Gallery route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import ListMovies from './movies'
-import AppBar from './components/AppBar/Appbar'
+import NavAppBar from './components/Navbar/AppNavBar'
 import GalleryShows from "./components/galleyShows/galleryShows";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import routes from './config/routes'; 
+
+
 function App() {
 
   return (
-    <div className="container">
-      <AppBar />
-      <GalleryShows />
-    </div>
+    <Router>
+      <NavAppBar />
+      <Switch>
+        <Route exact path="/gallery" render={props => <GalleryShows key={1} {...props} /> } />
+        {/* <Route exact from="/" render={props => <Home {...props} />} /> */}
+        {/* {routes.map((route, index) => {
+          return <RouteSubRoutes key={index} {...route} />
+        })} */}
+      </Switch>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import NavBar from '../AppBar/Appbar';
+const Home = () => {
+    return (
+        <div>
+            <NavBar />
+        </div>
+    )
+}
+
+export default Home;
