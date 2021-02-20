a269e0d933a9a91b06da80de8271de3cb5dc2108
Alfredo Rivas Jimenez <alfredo_rivas_jimenez@outlook.com>
2021-02-19 18:47:37 -0600

    [FIXED] Routes 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { lazy, Suspense } from 'react';
 import NavAppBar from './components/Navbar/AppNavBar';
 import Footer from './components/Footer/Footer';
-import LazyGalleryShows from './components/galleyShows/LazyGalleryShows';
-import ShowDetails from './components/galleyShows/showDetails';
+import LazyGalleryShows from './components/galleryShows/LazyGalleryShows';
+import ShowDetails from './components/galleryShows/showDetails';
 import Home from './components/Home/Home';
 import './App.css';
-import './components/galleyShows/showdetail.scss';
+import './components/galleryShows/showdetail.scss';
 import './components/Footer/FooterGrid.scss';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
